1da01faace8c6183c337d85cbd3defa940ca3882
jsusmachaca <falcorgd@gmail.com>
2024-10-08 10:42:39 -0500

    update ejem1

--- app1/ejem1.php
+++ app1/ejem1.php
@@ -4,6 +4,8 @@ $isDev = true;
 $age = 21;
 $isYoung = $age < 25;
 const BRO = 'SI PUES BRO';
+
+$array = ["BRO", "NONONO", "PPEPEPPE", "COME ALGO SONSO"]
 ?>
 
 <!-- <?php if ($isYoung) : ?>
@@ -17,6 +19,12 @@ const BRO = 'SI PUES BRO';
 
 <h1>Name: <?= $name; ?></h1>
 <h1>Is DEV: <?= $isDev; ?></h1>
-<h1>Age: <?= $age; ?></h1>
+<h1>Age: <?php echo $age; ?></h1>
 <h1>Bro: <?= BRO; ?></h1>
 <h1>Is young: <?= $isYoung; ?></h1>
+
+<ul>
+    <?php foreach ($array as $index => $element) : ?>
+        <li>[<?= $index; ?>] => <?= $element; ?></li>
+    <?php endforeach ?>
+</ul>
